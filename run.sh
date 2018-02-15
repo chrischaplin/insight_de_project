3685bd6e33957672cc566e941bfb78e2b487e4c8
chrischaplin <mxchap@gmail.com>
2018-02-15 10:31:59 -0800

    test-branch-one:  * added tacos  * added nachoes  * and some more garbage
    new message test
    
    one more hella dumb change

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 python ./src/Generate_Anomaly_List.py ./log_input/batch_log.json ./log_input/stream_log.json ./log_output/flagged_purchases.json
+
+echo tacos
+echo nachoes
+echo one
+echo more
